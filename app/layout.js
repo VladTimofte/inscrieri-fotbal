d7a50177a4f3bd221653ac8e931bea2409b0c679
VladTimofte <vladtimofte999@gmail.com>
2025-02-23 21:59:03 +0200

    favico

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import "../styles/globals.css";
 export const metadata = {
   title: "Înscriere Fotbal",
   description: "Aplicație pentru înscrierea la fotbal",
+  icons: {
+    icon: "/favico.png", // Calea către favicon
+  },
 };
 
 export default function RootLayout({ children }) {
